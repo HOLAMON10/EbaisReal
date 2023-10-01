a59c6da880af2d5a94e3acf7c647798cf97e66f9
Jose <Jose@DESKTOP-UB9REI4.mshome.net>
2023-09-30 18:29:05 -0700

    Demool

--- src/main/java/com/mycompany/ebaiss/Ebaiss.java
+++ src/main/java/com/mycompany/ebaiss/Ebaiss.java
@@ -11,6 +11,6 @@ package com.mycompany.ebaiss;
 public class Ebaiss {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello !");
     }
 }
